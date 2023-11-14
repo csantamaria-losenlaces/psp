b01f3a9ea27c7a430eab7c0d804617cab195c9e1
Carlos Santamaría <csantamaria.losenlaces@gmail.com>
2023-11-14 13:19:08 +0100

    Completado "Ej07_Patines". No se han contemplado patines individuales en la lógica, sino pares.

---
+++ Ej07_Patines/src/Alumno.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.Semaphore;
+
+public class Alumno extends Thread {
+
+	// Declaración de atributos
+	private int talla;
+	Semaphore patinesDisponibles;
+	
+	// Método constructor
+	public Alumno(String n, int t, Semaphore s) {
+		super(n); // Hereda el atributo nombre de Thread
+		this.talla = t;
+		this.patinesDisponibles = s;
+	}
+	
+	// Método que se ejecutará al lanzar el hilo
+	public void run() {
+		System.out.println(this.getName() + " esperando...");
+		try {
+			patinesDisponibles.acquire(1); // Se comprueba si hay un par de patines disponibles
+			System.out.println(this.getName() + " solicita talla " + talla + ". Queda/n " + patinesDisponibles.availablePermits() + " par/es");
+			sleep((int) (Math.random() * (2500 - 250) + 250)); // El alumno patina entre 0,25s y 2,5s
+			patinesDisponibles.release(); // Se libera el par de patines
+			System.out.println(this.getName() + " sale de la pista (deja patines talla " + talla + ")");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
--- Ej07_Patines/src/Principal.java
+++ Ej07_Patines/src/Principal.java
@@ -1,3 +1,6 @@
+import java.util.TreeMap;
+import java.util.concurrent.Semaphore;
+
 /* En una academia de patinaje tienen patines de los números 34 hasta el 43, dos pares de
  * cada número. Los alumnos de iniciación sólo requieren un patín, siendo siempre este el del
  * pie derecho. Se pide controlar la entrega de patines, según los van solicitando. Las
@@ -8,6 +11,31 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		// Declaración de variables
+		final int NUMALUMNOS = 10; // Número de alumnos
+		final int TALLAMIN = 34; // Talla más pequeña
+		final int TALLAMAX = 43; // Talla más grande
+		final int PARESPORTALLA = 2; // Pares de patines por talla
+		
+		Alumno[] alumnos = new Alumno[NUMALUMNOS]; // Array que contiene objetos "Alumno"
+		TreeMap<Integer, Semaphore> paresPatines = new TreeMap<>(); // TreeMap con la talla como clave y el semáforo como valor
+		
+		// Se rellena el TreeMap "paresPatines"
+		for (int t = TALLAMIN; t <= TALLAMAX; t++) {
+			Semaphore patinesDisponibles = new Semaphore(PARESPORTALLA); // Se crea un semáforo para cada talla con los pares disponibles
+			paresPatines.put(t, patinesDisponibles); // Se añade al TreeMap un par clave/valor nuevo
+		}
+		
+		// Se rellena el array "alumnos"
+		for (int i = 0; i < alumnos.length; i++) {
+			int tallaAleatoria = (int) (Math.random() * (43 - 34) + 34); // Talla aleatoria entre 34 y 43
+			alumnos[i] = new Alumno("Alumno " + i, tallaAleatoria, paresPatines.get(tallaAleatoria)); // Instanciación de un nuevo objeto "Alumno"
+		}
+		
+		// Se inician los hilos
+		for (Alumno a : alumnos) {
+			a.start();
+		}
 		
 		
 	}
