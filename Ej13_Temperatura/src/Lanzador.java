eaf92ac6ed743f26561bd589a13bdafddd9a323a
Carlos <Carlos@Sobremesa>
2023-11-26 16:04:38 +0100

    Arreglado fallo cálculo media de temperaturas y comentado
    código programa Ej13_Temperatura_Clase

--- Ej13_Temperatura/src/Lanzador.java
+++ Ej13_Temperatura/src/Lanzador.java
@@ -5,40 +5,45 @@ import java.io.InputStream;
 public class Lanzador {
 
 	public static void main(String[] args) {
-		
-		int suma = 0;
-		final int NUMCIUDADES = 20;
-		
-		for (int i = 0; i < NUMCIUDADES; i++) {
-			lanzarProceso(String.valueOf(((int)(Math.random() * 50)) + 50));
+
+		// Declaración de variables
+		double suma = 0;
+		final int NUMCIUDADES = 5;
+
+		// Bucle que se ejecutará NUMCIUDADES veces
+		for (int i = 0; i < NUMCIUDADES; i++) { 
+			int tempCiudad = lanzarProceso(String.valueOf(((int) (Math.random() * 50)) + 50)); // Lanza método con código postal aleatorio y guarda resultado en tempCiudad
+			suma += tempCiudad;
 		}
-		
-		System.out.println("La media de las ciudades es " + (double) (suma) / (NUMCIUDADES));
+
+		System.out.println("La media de las ciudades es " + (suma / NUMCIUDADES));
 
 	}
-	
+
+	// Método que lanza un proceso para obtener la temperatura de una ciudad
 	public static Integer lanzarProceso(String ciudad) {
-		File f = new File(".\\bin");
+
+		// Declaración de variables
+		File f = new File(".\\bin"); // Representa el directorio ".\bin"
 		String resultado = "";
-		
-		ProcessBuilder pb = new ProcessBuilder("java", "Temperatura", ciudad);
-		pb.directory(f);
-		
+
+		ProcessBuilder pb = new ProcessBuilder("java", "Temperatura", ciudad); // Crea un objeto ProcessBuilder para ejecutar el programa "Temperatura" con argumento "ciudad"
+		pb.directory(f); // Establece el directorio de trabajo del proceso
+
 		try {
+			// Intenta iniciar el proceso y obtener su flujo de entrada
 			Process p = pb.start();
 			InputStream is = p.getInputStream();
-			
 			int letra = is.read();
+			// Lee bytes del flujo de entrada hasta encontrar el carácter de retorno de carro (13 en ASCII)
 			while (letra != 13) {
-				resultado += (char)(letra);
+				resultado += (char) (letra);
 				letra = is.read();
 			}
 			System.out.println("La ciudad " + ciudad + " tiene una temperatura de " + resultado);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		return Integer.parseInt(resultado);
+		return Integer.parseInt(resultado); // Devuelve la temperatura
 	}
-
 }
--- Ej13_Temperatura/src/Temperatura.java
+++ Ej13_Temperatura/src/Temperatura.java
@@ -1,21 +1,23 @@
 public class Temperatura {
 
 	public static void main(String[] args) {
-		
+
+		// Declaración de variables
 		int temp;
-		
+
+		// Verifica si la cantidad de argumentos proporcionados no es igual a 1
 		if (args.length != 1) {
 			System.out.println("Error, se debe introducir un solo argumento");
 		} else {
-			temp = ((int) (Math.random() * 50)) - 10;
+			temp = ((int) (Math.random() * 50)) - 10; // Genera un número aleatorio entre -10 y 39
 			try {
-				Thread.sleep(500);
+				Thread.sleep(500); // Pausa la ejecución durante 500ms
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			System.out.println(temp);
 		}
-		
+
 	}
-	
+
 }
