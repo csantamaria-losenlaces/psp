d9c200c376f116a9e293f0241b0d5fe0ef0fffc4
CarlosSantamaria <csantamaria.losenlaces@gmail.com>
2023-12-04 16:57:47 +0100

    Agregado ejercicio finalizado "2023-12-04_Biblioteca" y
    realizados cambios menores en "2023-12-04_ProduccionFabrica" (versión
    original y corregida)

--- 2023-12-04_ProduccionFabrica_Correccion/src/Fabrica.java
+++ 2023-12-04_ProduccionFabrica_Correccion/src/Fabrica.java
@@ -1,17 +1,35 @@
+/* Crea un sistema que simule la producción y consumo de productos en una fábrica. La fábrica
+ * tiene una capacidad para almacenar hasta 20 productos. Tienes dos tipos de trabajadores:
+ * productores y consumidores. Los productores generan productos y los colocan en el almacén,
+ * mientras que los consumidores toman productos del almacén. Debes garantizar que el
+ * almacén no supere su capacidad máxima y que los consumidores no intenten tomar productos
+ * cuando el almacén está vacío. Utiliza el modelo productor-consumidor con semáforos para
+ * sincronizar la producción y el consumo. */
+
 public class Fabrica {
     public static void main(String[] args) {
-        Almacen almacen = new Almacen();
+        
+    	final int NUMEMPLEADOS = 100;
+    	Almacen almacen = new Almacen();
 
         // Crear productores y consumidores
-        Productor productor1 = new Productor(almacen);
-        Productor productor2 = new Productor(almacen);
-        Consumidor consumidor1 = new Consumidor(almacen);
-        Consumidor consumidor2 = new Consumidor(almacen);
+        Productor[] arrayProductores = new Productor[NUMEMPLEADOS];
+        Consumidor[] arrayConsumidores = new Consumidor[NUMEMPLEADOS];
+        
+        for (int i = 0; i < NUMEMPLEADOS; i++) {
+        	arrayProductores[i] = new Productor(almacen);
+        	arrayConsumidores[i] = new Consumidor(almacen);
+		}
+        
 
         // Iniciar los hilos
-        productor1.start();
-        productor2.start();
-        consumidor1.start();
-        consumidor2.start();
+        for (Productor p : arrayProductores) {
+			p.start();
+		}
+        
+        for (Consumidor c : arrayConsumidores) {
+			c.start();
+		}
+        
     }
 }
